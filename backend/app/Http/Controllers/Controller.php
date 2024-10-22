592a97494c5f1b3bc68804d59f1e0e1e860de115
Gabriela <127972306+gabriela-vitor@users.noreply.github.com>
2024-10-22 12:21:05 -0300

    Controller.php

--- backend/app/Http/Controllers/Controller.php
+++ backend/app/Http/Controllers/Controller.php
@@ -10,15 +10,18 @@ class TransactionController extends Controller
     }
 
     public function store(Request $request) {
-        $request->validate([
-            'data' => 'required|date',
-            'valor' => 'required|numeric',
-            'categoria' => 'required|string|max:50',
-            'tipo' => 'required|exists:transaction_types,id',
-        ]);
+    // Validação dos dados recebidos na requisição
+    $request->validate([
+        'data' => 'required|date', // Verifica se o campo "data" é obrigatório e está no formato de data
+        'valor' => 'required|numeric', // Verifica se o campo "valor" é obrigatório e é numérico
+        'categoria' => 'required|string|max:50', // Verifica se o campo "categoria" é obrigatório, é uma string e tem no máximo 50 caracteres
+        'tipo' => 'required|exists:transaction_types,id', // Verifica se o campo "tipo" é obrigatório e se existe uma correspondência no banco de dados na tabela "transaction_types" com base no "id"
+    ]);
 
-        return Transaction::create($request->all());
-    }
+    // Criação da transação com os dados validados
+    return Transaction::create($request->all()); // Cria uma nova entrada no banco de dados usando os dados da requisição validados
+}
+
 
     public function update(Request $request, $id) {
         $transaction = Transaction::findOrFail($id);
