dd79d41a0f189a1daad285ab021fd3daebfa453b
Gabriela <127972306+gabriela-vitor@users.noreply.github.com>
2024-10-22 12:16:11 -0300

    app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { TransactionFormComponent } from './transactions/transaction-form/transaction-form/transaction-form.component';
+import { TransactionListComponent } from './transactions/transaction-list/transaction-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/transactions', pathMatch: 'full' },  // Redireciona o caminho vazio para a lista de transações
+  { path: 'transactions', component: TransactionListComponent },  // Rota para a lista de transações
+  { path: 'add-transaction', component: TransactionFormComponent },  // Rota para adicionar uma nova transação
+  { path: 'edit-transaction/:id', component: TransactionFormComponent }  // Rota para editar uma transação, passando o ID
+];
+
