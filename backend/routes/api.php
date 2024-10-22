88a9326e0b694751842831aa9155f74f614de921
Gabriela <127972306+gabriela-vitor@users.noreply.github.com>
2024-10-22 16:52:12 -0300

    api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\TransactionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/transactions', [TransactionController::class, 'index']);
 Route::post('/transactions', [TransactionController::class, 'store']);
